72c34997d0fc2a0d0c0ec253fdd7a18c73d789ad
zarbinatti <165228044+zarbinatti@users.noreply.github.com>
2025-06-09 18:59:29 -0300

    m

--- main.js
+++ main.js
@@ -18,3 +18,26 @@ tamanhoSenha++;
 }
 numeroSenha.textContent = tamanhoSenha;
 }
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+
+function geraSenha() {
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
