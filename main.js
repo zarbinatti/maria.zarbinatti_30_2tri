cde953df514c49d4ad81f618d3c895bc48ff5573
zarbinatti <165228044+zarbinatti@users.noreply.github.com>
2025-06-16 19:16:15 -0300

    m

--- main.js
+++ main.js
@@ -28,12 +28,9 @@ function aumentaTamanho() {
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-
 const campoSenha = document.querySelector('#campo-senha');
-
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 geraSenha();
-
 function geraSenha() {
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
@@ -42,4 +39,16 @@ function geraSenha() {
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
     campoSenha.value = senha;
+}
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+function geraSenha() {
+let senha = '';
+for (let i = 0; i < tamanhoSenha; i++) {
+let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+numeroAleatorio = Math.floor(numeroAleatorio);
+senha = senha + letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value = senha;
 }
